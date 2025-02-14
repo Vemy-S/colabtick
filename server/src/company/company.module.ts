93e523a30a92cd38e8189e716a5e198ad3d4f455
Jeremy Vega <jeredev20@gmail.com>
2025-02-14 18:24:09 -0300

    mailgun with nodemailer free

--- server/src/company/company.module.ts
+++ server/src/company/company.module.ts
@@ -5,11 +5,12 @@ import { PrismaService } from 'src/libs/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { CompanyInvitationController } from './controllers/company-invitation.controller';
 import { CompanyInvitationService } from './services/company-invitation.service';
+import { MailInvitationService } from './services/mail-invitation.service';
 
 
 @Module({
   imports: [],
-  providers: [CompanyService, PrismaService, JwtService, CompanyInvitationService],
+  providers: [CompanyService, PrismaService, JwtService, CompanyInvitationService, MailInvitationService],
   controllers: [CompanyController, CompanyInvitationController]
 })
 export class CompanyModule {}
